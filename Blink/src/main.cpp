cd1a6dbdde2483de228e708cc5c9e4bbc385f15f
Alvin Abad <alvnabad@gmail.com>
2021-10-20 15:55:21 -0700

    Add Blink

---
+++ Blink/src/main.cpp
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ *
+ * Compute for Pi and display on ssd1306 128x64 i2c OLED
+ *
+ * MIT License
+ *
+ * Copyright (c) 2021 Alvin Abad
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ ******************************************************************************/
+
+/*
+  Blink
+*/
+
+#include <Arduino.h>
+#include <WiFiNINA.h>
+
+#define DELAY           500
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+    Serial.begin(115200);
+    delay(2000);
+
+    Serial.println();
+    Serial.print("Begin BLINKER LED_BUILTIN and LED RGB: ");
+    Serial.println(LED_BUILTIN);
+
+    // initialize digital pin LED_BUILTIN as an output.
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(LEDR, OUTPUT);
+    pinMode(LEDG, OUTPUT);
+    pinMode(LEDB, OUTPUT);
+}
+
+void loop() {
+    Serial.print("LED_BUILTIN ON: ");
+    Serial.println(LED_BUILTIN);
+
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(DELAY);
+  
+    Serial.print("LED_BUILTIN OFF: ");
+    Serial.println(LED_BUILTIN);
+
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(LEDR, HIGH);
+    delay(DELAY);
+
+    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(LEDR, LOW);
+    digitalWrite(LEDG, HIGH);
+    delay(DELAY);
+
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(LEDG, LOW);
+    digitalWrite(LEDB, HIGH);
+    delay(DELAY);
+
+    digitalWrite(LEDB, LOW);
+}
